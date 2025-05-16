af5161f33513ce0f143684a7f156ff34869c6d58
maldikurniawan <akurniawan2102@gmail.com>
2025-05-16 09:44:13 +0700

    fix loader

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,39 +1,26 @@
 import React, { Fragment, useEffect, useState } from "react";
-import { Sidebar, Header, Footer, Loader } from "@/components";
+import { Sidebar, Header, Footer } from "@/components";
 import { Outlet } from "react-router-dom";
 
 const Layout: React.FC = () => {
-    const [sideOpen, setSideOpen] = useState<boolean>(false);
-    const [loading, setLoading] = useState<boolean>(true);
+    const [sideOpen, setSideOpen] = useState<boolean>(true);
 
     useEffect(() => {
-        const timeout = setTimeout(() => {
-            setLoading(false);
-        }, 100);
-
         setSideOpen(window.innerWidth >= 767);
-
-        return () => clearTimeout(timeout);
     }, []);
 
     return (
         <Fragment>
-            {loading ? (
-                <div className="flex justify-center items-center h-screen bg-black">
-                    <Loader />
-                </div>
-            ) : (
-                <div className="flex">
-                    <Sidebar sideOpen={sideOpen} setSideOpen={setSideOpen} />
-                    <div className="w-full h-screen flex flex-col relative">
-                        <Header sideOpen={sideOpen} setSideOpen={setSideOpen} />
-                        <div className="bg-black h-full overflow-y-auto px-4 py-4 text-white scroll-hidden">
-                            <Outlet />
-                        </div>
-                        <Footer />
+            <div className="flex">
+                <Sidebar sideOpen={sideOpen} setSideOpen={setSideOpen} />
+                <div className="w-full h-screen flex flex-col relative">
+                    <Header sideOpen={sideOpen} setSideOpen={setSideOpen} />
+                    <div className="bg-black h-full overflow-y-auto px-4 py-4 text-white scroll-hidden">
+                        <Outlet />
                     </div>
+                    <Footer />
                 </div>
-            )}
+            </div>
         </Fragment>
     );
 };
